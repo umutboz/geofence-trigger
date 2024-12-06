5d7f17e430c0be32a0f36886f19548000a6a853f
Umut Boz <umut.boz@outlook.com>
2024-12-06 23:20:55 +0300

    change structure

---
+++ Sources/GeofenceTrigger/GeofenceTrigger.h
@@ -0,0 +1,18 @@
+//
+//  GeofenceTrigger.h
+//  GeofenceTrigger
+//
+//  Created by umud on 6.12.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeofenceTrigger.
+FOUNDATION_EXPORT double GeofenceTriggerVersionNumber;
+
+//! Project version string for GeofenceTrigger.
+FOUNDATION_EXPORT const unsigned char GeofenceTriggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeofenceTrigger/PublicHeader.h>
+
+
