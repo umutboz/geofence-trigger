bb63c8d7550c4ce0d24a98bc5f4f576458de4156
Umut Boz <umut.boz@outlook.com>
2024-12-07 11:47:34 +0300

    LocationData add init

--- Sources/GeofenceTrigger/LocationData.swift
+++ Sources/GeofenceTrigger/LocationData.swift
@@ -7,10 +7,18 @@
 
 import Foundation
 public struct LocationData{
+    public init(name:String, coordinate :Coordinate) {
+        self.name = name
+        self.coordinate = coordinate
+    }
     public var name : String
     public var coordinate : Coordinate
 }
 public struct Coordinate{
+    public init(latitude:Double, longitude:Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
     public var latitude: Double
     public var longitude: Double
 }
